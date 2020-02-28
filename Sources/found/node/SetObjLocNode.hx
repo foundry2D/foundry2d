032a4d4237d5a3865e5ca90d720ffd6ef7335b60
Bernard Lambert <bernardlambert2012@gmail.com>
2020-02-28 12:54:06 -0500

    Fix SetObjLocNode to use correct input to set object position + reformat SetObjLocNode code

--- Sources/found/node/SetObjLocNode.hx
+++ Sources/found/node/SetObjLocNode.hx
@@ -3,21 +3,19 @@ package found.node;
 import found.object.Object.MoveData;
 import kha.math.FastVector2;
 
-class SetObjLocNode extends LogicNode{
-
-	
-    public function new(tree:LogicTree) {
+class SetObjLocNode extends LogicNode {
+	public function new(tree:LogicTree) {
 		super(tree);
 	}
 
 	override function run(from:Int) {
-		var pos:FastVector2 = inputs[1].get();
+		var pos:FastVector2 = inputs[2].get();
 
-        tree.object.translate(function(data:MoveData){
-            data._positions.x = pos.x;
-            data._positions.y = pos.y;
-            return data;
-        });
+		tree.object.translate(function(data:MoveData) {
+			data._positions.x = pos.x;
+			data._positions.y = pos.y;
+			return data;
+		});
 
 		runOutput(0);
 	}

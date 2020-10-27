41d07740eed42aba49eb55ebf126f7faad4fb842
mundusnine <mundusnine@gmail.com>
2020-10-27 15:04:01 -0400

    Fixed GetForwardNode. Added dbeug define for nodes(so we can activate or deactivate debug view code for specific nodes).

--- Sources/found/node/GetForwardNode.hx
+++ Sources/found/node/GetForwardNode.hx
@@ -1,16 +1,29 @@
 package found.node;
 
+import found.math.Util;
 import kha.math.Vector2;
 import found.object.Object;
 
 class GetForwardNode extends LogicNode {
+	#if debug_nodes
+	public function new(tree:LogicTree) {
+		super(tree);
+		tree.notifyOnRender2D(debugDraw);
+	}
+	function debugDraw(g2:kha.graphics2.Graphics){
+		var pos:Vector2 = tree.object.center;
+		var forward:Vector2  = this.get(0);
+		forward = forward.mult(50);
+		g2.color = 0xaa0000ff;
+		g2.fillRect(pos.x + forward.x,pos.y + forward.y,10,10);
+		g2.color = 0xffffffff;
+	}
+	#end
 	override function get(from:Int):Dynamic {
 		
-		if (inputs[0].node == null) {
-			return new Vector2(Math.cos(tree.object.rotation.z),Math.sin(tree.object.rotation.z));
-		} else {
-			var object:Object = cast(inputs[0].get());
-			return new Vector2(Math.cos(object.rotation.z),Math.sin(object.rotation.z));
-		}
+		var angle = Util.degToRad(inputs[0].node == null ? 
+		tree.object.rotation.z:
+		cast(inputs[0].get(),Object).rotation.z);
+		return new Vector2(Math.cos(angle),Math.sin(angle));
 	}
 }
--- khafile.js
+++ khafile.js
@@ -10,6 +10,10 @@ project.addParameter("--macro keep('found.node')");
 project.addParameter('--macro echo.Macros.add_data("object", "found.object.Object")');
 // @TODO: Fix strict null safety issues by enabling this and fixing the compile issues
 // project.addParameter('--macro nullSafety("found", Strict)');
+
+// To enable debug code for nodes:
+// project.addDefine("debug_nodes");
+
 project.addLibrary('Libraries/foundsdk/hxmath');
 project.addLibrary('Libraries/foundsdk/echo');
 project.addLibrary('Libraries/foundsdk/zui');
